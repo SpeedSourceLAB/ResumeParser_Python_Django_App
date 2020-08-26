3865757fe32150ea886726baa16ea3fff6b3fd15
SpeedSourceLAB <pradeepa.gp@gmail.com>
2020-08-26 15:51:02 -0500

    Updated the views.py

--- ResumePortal/ResumePortal/settings.py
+++ ResumePortal/ResumePortal/settings.py
@@ -148,17 +148,12 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = ["C:\\Users\\prade\\PycharmProjects\\ResumePortal\\resumeapp\\static",]
-STATICFILES_DIRS = ["C:\\Users\\prade\\PycharmProjects\\ResumePortal\\resumeapp\\templates\\resumeapp",]
-#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-#AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-#AWS_S3_FILE_OVERWRITE = False
-#AWS_DEFAULT_ACL = None
 
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_DIRS = ["C:\\Users\\prade\\PycharmProjects\\ResumePortal\\resumeapp\\templates\\resumeapp",]
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-#MEDIA_ROOT = "C:\\Users\\prade\\PycharmProjects\\ResPortal\\resumeapp\\media"
+
 
 MEDIA_URL = '/media/'
 
